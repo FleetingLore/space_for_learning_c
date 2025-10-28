1d244ecbaac3143b90bb58c227142861c14e29be
Mon Cera <Macro__9@outlook.com>
2025-10-28 15:18:27 +0000

    0

--- r/2/3.c
+++ r/2/3.c
@@ -7,32 +7,40 @@ abcdefghijklmnopqrstuvwxyz\
 ";
 
 /**
- * [arg - '0' + 10]
- * 0123456789   |
- * ||||||||||   |
- * ABCDEFGHIJ   |
- *              |
- * [arg - 'A' + 10 + 10]
- * ABCDEFGHIJKLMN0P
- * ||||||||||||||||
- * KLMN0PQRSTUVWXYZ
- * 
- * QRSTUVWXYZ  abcdefghijklmno  qrstuvwxyz
- * ||||||||||  |||||||||||||||  ||||||||||
- * abcdefghij  klmnoqrstuvwxyz  0123456789
- * 
- * 
- * 
- * 
- * 
+ * [-'0'+10]
+ * 01..9 |
+ * ||  | | 
+ * AB..J | 
+ *       |           
+ * [-'A'+10+16]
+ * AB..P    |
+ * ||  |    |
+ * KL..Z    |
+ *          |        
+ * [-'Q'+10+16+10]
+ * QR..Z       |
+ * ||  |       |     
+ * ab..j       |     
+ *             |          
+ * [-'a'+10+16+10+16]
+ * ab..o          |
+ * ||  |          |
+ * kl..z          |
+ *                |
+ * [-'q'+10+16+10+16+10]
+ * qr..z
+ * ||  |
+ * 01..9
  */
 
 /* public */ char
 f_0_(char arg) {
-    return map[(arg >= '0' && arg <= '9') ? (arg - '0' + 10) :
-               (arg >= 'A' && arg <= 'Z') ? (arg - 'A' + 10 + 10) :
-               (arg >= 'a' && arg <= 'z') ? (arg - 'a' + 10 + 10 + 26) :
-               0
+    return map[
+        (arg >= '0' && arg <= '9') ? (arg - '0' + 10) :
+        (arg >= 'A' && arg <= 'P') ? (arg - 'A' + 26) :
+        (arg >= 'Q' && arg <= 'Z') ? (arg - 'Q' + 36) :
+        (arg >= 'a' && arg <= 'o') ? (arg - 'a' + 52) :
+        (arg >= 'q' && arg <= 'z') ? (arg - 'q') : 0
     ];
 }
 
